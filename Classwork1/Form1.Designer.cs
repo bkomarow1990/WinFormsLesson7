ce5106a7f4ba7917f896eda427a5d756ec92f6dc
Bogdan <71147719+bkomarow1990@users.noreply.github.com>
2021-05-09 00:43:14 +0300

    Add project files.

---
+++ Classwork1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Classwork1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorBtn = new System.Windows.Forms.Button();
+            this.fontBtn = new System.Windows.Forms.Button();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // colorBtn
+            // 
+            this.colorBtn.Location = new System.Drawing.Point(12, 12);
+            this.colorBtn.Name = "colorBtn";
+            this.colorBtn.Size = new System.Drawing.Size(104, 58);
+            this.colorBtn.TabIndex = 0;
+            this.colorBtn.Text = "Color";
+            this.colorBtn.UseVisualStyleBackColor = true;
+            this.colorBtn.Click += new System.EventHandler(this.colorBtn_Click);
+            // 
+            // fontBtn
+            // 
+            this.fontBtn.Location = new System.Drawing.Point(12, 104);
+            this.fontBtn.Name = "fontBtn";
+            this.fontBtn.Size = new System.Drawing.Size(104, 58);
+            this.fontBtn.TabIndex = 1;
+            this.fontBtn.Text = "Font";
+            this.fontBtn.UseVisualStyleBackColor = true;
+            this.fontBtn.Click += new System.EventHandler(this.fontBtn_Click);
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(12, 192);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(104, 58);
+            this.openBtn.TabIndex = 2;
+            this.openBtn.Text = "Open File";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.Location = new System.Drawing.Point(123, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ShowSelectionMargin = true;
+            this.richTextBox1.Size = new System.Drawing.Size(641, 372);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "Demo Rich TextBox";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(12, 268);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(104, 58);
+            this.saveBtn.TabIndex = 4;
+            this.saveBtn.Text = "Save file";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.fontBtn);
+            this.Controls.Add(this.colorBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button colorBtn;
+        private System.Windows.Forms.Button fontBtn;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Classwork1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classwork1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //this.richTextBox1.sele
+        }
+
+        private void colorBtn_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionColor = colorDialog1.Color;
+            }
+            
+            
+        }
+
+        private void fontBtn_Click(object sender, EventArgs e)
+        {
+            fontDialog1.Color = Color.Black;
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog1.Font;
+                richTextBox1.SelectionColor = fontDialog1.Color;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Txt files(*.txt)|*.txt|Rich files(*.rtf)||All files(*.*)|*.*||";
+            openFileDialog1.FilterIndex = 3;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                switch (openFileDialog1.FilterIndex)
+                {
+                   
+                    case 1:
+                    case 3:
+                    case 0:
+                        richTextBox1.Text =  File.ReadAllText(path);
+                        break;
+                    case 2:
+                        if (Path.GetExtension(path) == ".rtf")
+                        {
+                            richTextBox1.LoadFile(path, RichTextBoxStreamType.RichText);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            string path;
+            saveFileDialog1.Filter = "Txt files(*.txt)|*.txt|Rich files(*.rtf)||All files(*.*)|*.*||";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path = saveFileDialog1.FileName;
+                switch (Path.GetExtension(path))
+                {
+                    case ".txt":
+                        File.WriteAllText(path, richTextBox1.Text);
+                        break;
+                    case ".rtf":
+                        richTextBox1.SaveFile(path, RichTextBoxStreamType.RichText);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
